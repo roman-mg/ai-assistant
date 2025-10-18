24c06e10c95e80f9d075ac1ce0de06fee6187349
RM <23095125+roman-mg@users.noreply.github.com>
2025-10-18 23:29:31 +0200

    fix import error

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,7 +2,6 @@
 
 from .multi_agent_orchestrator import multi_agent_orchestrator
 from .query_analysis_agent import query_analysis_agent
-from .researcher_agent import researcher_agent
 from .search_agent import search_agent
 from .security_agent import security_agent
 from .summary_agent import summary_agent
@@ -10,7 +9,6 @@ from .summary_agent import summary_agent
 __all__ = [
     "multi_agent_orchestrator",
     "query_analysis_agent",
-    "researcher_agent",
     "search_agent",
     "security_agent",
     "summary_agent",
